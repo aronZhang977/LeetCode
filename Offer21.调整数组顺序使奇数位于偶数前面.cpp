965235a3dfe0e767cee20586b437a8e5163cff5b
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-07 19:18:15 +0800

    0407

--- Offer21.调整数组顺序使奇数位于偶数前面.cpp
+++ Offer21.调整数组顺序使奇数位于偶数前面.cpp
@@ -1,6 +1,23 @@
+// 首尾指针 原地交换
 class Solution {
 public:
     vector<int> exchange(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
 
-    }
+        while(left < right)
+        {
+            if (nums[left] & 0x1)
+            {
+                ++left;
+                continue;
+            }
+            else if(!(nums[right] & 0x1))
+            {
+                --right;
+                continue;
+            }
+            std::swap(nums[left++],nums[right--]);   
+        }
+        return nums;
+  }
 };
