83ec9655fbf8ceaa5a7f185041a9c0f1b6473ab3
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-13 13:42:40 +0800

    0413

--- offer33.二叉搜索树的后序遍历.cpp
+++ offer33.二叉搜索树的后序遍历.cpp
@@ -8,7 +8,7 @@ public:
         int p = start;
         int rootVal = postorder[end];
         // 找左子树结束的位置
-        while(postorder[p] <rootVal)
+        while(postorder[p] < rootVal)
             ++p;
         // 设置左子树结束位置
         int leftEnd = p - 1;
@@ -16,9 +16,9 @@ public:
         while(postorder[p] > rootVal)
             ++p;
         // 右子树结束位置应该正好在end这里
-        return p = end && dfs(postorder,start,leftEnd) && dfs(postorder,leftEnd + 1,end - 1);
+        return p == end && dfs(postorder,start,leftEnd) && dfs(postorder,leftEnd + 1,end - 1);
     }
-    
+
     bool verifyPostorder(vector<int>& postorder) {
         return dfs(postorder, 0 ,postorder.size() - 1);        
     }
