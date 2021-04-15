76950266c836c54c9005ea4ed654081c7150e74e
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-15 10:54:29 +0800

    0415

--- offer36.二叉搜索树与双向链表.cpp
+++ offer36.二叉搜索树与双向链表.cpp
@@ -1,5 +1,10 @@
 /*
-// Definition for a Node.
+    二叉搜索树转换成一个 有序的循环双向链表
+
+    基于 二叉搜索树的中序遍历为递增序列
+
+*/
+
 class Node {
 public:
     int val;
@@ -20,10 +25,31 @@ public:
         right = _right;
     }
 };
-*/
+
+// 二叉搜索树转换成一个 有序的循环双向链表
 class Solution {
 public:
     Node* treeToDoublyList(Node* root) {
-        
+        if (!root)
+            return nullptr;
+        dfs(root);
+        m_pHead->left = m_pPre;
+        m_pPre->right = m_pHead;
+        return m_pHead;
+    }
+private:
+    Node *m_pPre,*m_pHead;
+    void dfs(Node *pCur)
+    {
+        if (nullptr == pCur)
+            return;
+        dfs(pCur->left);
+        if (nullptr != m_pPre)
+            m_pPre->right = pCur;
+        else
+            m_pHead = pCur;
+        pCur->left = m_pPre;
+        m_pPre = pCur;
+        dfs(pCur->right);        
     }
 };
---
+++ offer38.字符串的排列.cpp
@@ -0,0 +1,7 @@
+// 输入一个字符串，打印出该字符串中字符的所有排列。
+class Solution {
+public:
+    vector<string> permutation(string s) {
+
+    }
+};
