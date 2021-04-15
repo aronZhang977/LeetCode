9d906eca7208dfeb3408c75d505704eeb01ba50c
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-15 16:10:30 +0800

    0415

--- offer38.字符串的排列.cpp
+++ offer38.字符串的排列.cpp
@@ -2,6 +2,27 @@
 class Solution {
 public:
     vector<string> permutation(string s) {
-
+        dfs(s,0);
+        return res;
+    }
+private:
+    vector<string> res;
+    void dfs(string str, int x)
+    {
+        if( x == str.size() - 1)
+        {
+            res.push_back(str);   //添加排列方案
+            return;
+        }
+        set<int> set;               // 用set来查重
+        for(int i = x; i < s.size(); ++i)
+        {
+            if(set.find(str[i]) != set.end())
+                continue;                   // 重复， 剪枝
+            set.insert(str[i]);
+            swap(str[i],str[x]);        // 交换，将s[i]固定在第x位
+            dfs(str,x + 1);             // 开启固定第x+1位字符
+            swap(str[i],str[x]);        // 恢复交换
+        }
     }
 };
