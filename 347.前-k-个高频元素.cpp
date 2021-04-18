659e690d92b4f3dbadf7555c791f797c860af39d
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-18 09:47:23 +0800

    0418

--- 347.前-k-个高频元素.cpp
+++ 347.前-k-个高频元素.cpp
@@ -17,7 +17,7 @@ public:
     }
 
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int> map;   // nums , count
+        unordered_map<int,int> map;   // nums , count
         for(auto& i : nums)
         {
             ++map[i];
@@ -27,14 +27,14 @@ public:
         {
             ++map[nums[i]];
         }
-        /**
+        */
+        /*
          * 定义：priority_queue<Type, Container, Functional>
          * Type 就是数据类型，
          * Container 就是容器类型（Container必须是用数组实现的容器，比如vector,deque等等，但不能用 list。STL里面默认用的是vector），
          * Functional 就是比较的方式。当需要用自定义的数据类型时才需要传入这三个参数，使用基本数据类型时，只需要传入数据类型，默认是大顶堆。
         */
-
-        priority_queue<pair<int,int>,vector<int,int>,decltype(&cmp)> que(cmp); 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(&cmp)> que(cmp);
         for(auto& [num,count] : map)
         {
             // k个以后
@@ -51,9 +51,9 @@ public:
             }
         }
         vector<int> res;
-        while(!que.emoty())
+        while(!que.empty())
         {
-            res.emplace_back(que.top().frist);
+            res.emplace_back(que.top().first);
             que.pop();
         }
         return res;
--- 692.前k个高频单词.cpp
+++ 692.前k个高频单词.cpp
@@ -5,11 +5,110 @@
  */
 
 // @lc code=start
+
+#include<string>
+#include<unordered_map>
+
+// 写法1：
+
 class Solution {
 public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
+/*
+    1.遍历，使用哈希表统计词频
+    2.堆排
+*/
+    static bool cmp(pair<string,int>& m, pair<string,int>& n )
+    {
+        return m.second == n.second ? (m.first < n.first) : (m.second > n.second);
+    }
 
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> map;   
+        for(auto& word : words)
+        {
+            ++map[word];
+        }
+
+        priority_queue<pair<string,int>, vector<pair<string,int> >,decltype(&cmp)> que(cmp);
+        for(auto it = map.begin();it != map.end(); ++it)
+        {
+            que.emplace(*it);
+            if(que.size() > k)
+            {
+                que.pop();
+            }            
+        }
+        /*
+        vector<string> res;
+        while(!que.empty())
+        {
+            res.emplace_back(que.top().first);
+            que.pop();
+        }
+        reverse(res.begin(),res.end());
+        */
+        vector<string> res(k);
+        while(k-- > 0)
+        {
+            res[k] = que.top().first;
+            que.pop();
+        }   
+
+        return res;
     }
 };
+
+// 写法2:
+class MyCmp
+{
+public:
+    bool operator()(const pair<string,int>& m,const pair<string,int>& n)
+    {
+        return m.second == n.second ? (m.first < n.first) : (m.second > n.second);
+    }
+};
+
+class Solution {
+public:
+/*
+    1.遍历，使用哈希表统计词频
+    2.堆排
+*/
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> map;   
+        for(auto& word : words)
+        {
+            ++map[word];
+        }
+
+        priority_queue<pair<string,int>, vector<pair<string,int>>, MyCmp> que;
+        for(auto it = map.begin();it != map.end(); ++it)
+        {
+            que.emplace(*it);
+            if(que.size() > k)
+            {
+                que.pop();
+            }            
+        }
+        /*
+        vector<string> res;
+        while(!que.empty())
+        {
+            res.emplace_back(que.top().first);
+            que.pop();
+        }
+        reverse(res.begin(),res.end());
+        */
+        vector<string> res(k);
+        while(k-- > 0)
+        {
+            res[k] = que.top().first;
+            que.pop();
+        }   
+
+        return res;
+    }
+};
+
 // @lc code=end
 
