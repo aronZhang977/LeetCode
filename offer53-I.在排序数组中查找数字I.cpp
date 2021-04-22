2b51d9e3cb660406c525009ccdbef44b9140f0c2
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-22 13:47:02 +0800

    0422

--- offer53-I.在排序数组中查找数字I.cpp
+++ offer53-I.在排序数组中查找数字I.cpp
@@ -1,6 +1,36 @@
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+    //暴力遍历 
+    int times = 0;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i] == target) times++;
+        return times;
+    }
+};
 
+// 哈希
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        for(int num : nums){
+            ++map[num];
+        }
+        return map[target];
+    }
+};
+
+ 
+
+// stl
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto iter = lower_bound(nums.begin(), nums.end(), target);  //寻找下边界
+        auto iter2 = upper_bound(nums.begin(), nums.end(), target); //寻找上边界
+        return iter2 - iter;    // [iter,iter2) 左闭右开区间 
     }
 };
