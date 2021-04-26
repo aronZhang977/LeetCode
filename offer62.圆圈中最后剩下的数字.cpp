6a7a5f3e059de2d67900a4505439b6ba9541748c
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-26 10:27:03 +0800

    0426

--- offer62.圆圈中最后剩下的数字.cpp
+++ offer62.圆圈中最后剩下的数字.cpp
@@ -1,6 +1,28 @@
+// 数学+递归
+class Solution {
+public:
+    int func(int n,int m)
+    {
+        if(1 == n)
+            return 0;
+
+        int x = func(n-1,m);
+        return (m + x) % n;
+    }
+
+    int lastRemaining(int n, int m) {
+        return func(n,m);    
+    }
+};
+// 数学+迭代
 class Solution {
 public:
     int lastRemaining(int n, int m) {
-
+        int f = 0;
+        for(int i = 2; i != n + 1; ++i)
+        {
+            f = (m + f) % i;
+        }
+        return f;
     }
 };
