51971e241fa0fa28e609c56336425071da54e55e
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-26 10:06:27 +0800

    0426

--- offer61.扑克牌中的顺子.cpp
+++ offer61.扑克牌中的顺子.cpp
@@ -1,6 +1,37 @@
+//
 class Solution {
 public:
     bool isStraight(vector<int>& nums) {
+        vector<int> map(14);   // 用数组构建哈希集合
+        int minVale = INT_MAX, maxValue = INT_MIN;
+        for(int n : nums)
+        {
+            if(map[n] >= 1)
+                return false;   // 重复数字
+            if(0 == n)
+                continue;       // 大小王、跳过
+            minVale = min(minVale,n);
+            maxValue = max(maxValue,n);
+            ++map[n];
+        }
+        return maxValue - minVale <= 4;
+    }
+};
+
+// 排序
+class Solution {
+public:
+    bool isStraight(vector<int>& nums) {
+        int minIndex = 0;
+        sort(nums.begin(),nums,end());
+        for(int i = 0; i < nums.size() - 1; ++i)
+        {
+            if(0 == nums[i])
+                ++minIndex;
+            else if(nums[i] == nums[i + 1])
+                return false;
+        }
+        return nums[4] - nums[minIndex] <= 4;
 
     }
 };
