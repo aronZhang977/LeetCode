2430dd80e9efcf7b7631c2e80d28c7b5c141ad17
aronZhang977 <aron.zhang.coding@gmail.com>
2021-04-26 14:16:12 +0800

    0426

--- offer63.股票的最大利润.cpp
+++ offer63.股票的最大利润.cpp
@@ -1,6 +1,31 @@
+
+// 双指针 暴力遍历
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int n = prices.size(),res = 0;
+        for(int i = 0; i < n;++i)
+        {
+            for(int j = i + 1; j < n; ++j)
+            {
+                res = max(res,prices[j] - prices[i]);
+            }
+        }
+        return res;
+    }
+};
 
+
+// 一次遍历
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = INT_MAX,maxprofit = 0;
+        for(int price : prices)
+        {
+            maxprofit = max(maxprofit,price - minprice);
+            minprice = min(price,minprice);
+        }
+        return maxprofit;
     }
 };
