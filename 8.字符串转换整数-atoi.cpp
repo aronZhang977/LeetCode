e96905378f26ac685547f528ab8b381953868bd7
aronZhang977 <aron.zhang.coding@gmail.com>
2021-06-29 14:39:31 +0800

    0629

--- 8.字符串转换整数-atoi.cpp
+++ 8.字符串转换整数-atoi.cpp
@@ -7,8 +7,34 @@
 // @lc code=start
 class Solution {
 public:
-    int myAtoi(string s) {
-
+    int myAtoi(string str) {
+        int res = 0, i = 0;
+        bool flag = true;
+        // 过空格
+        while(str[i] == ' ')
+        {
+            ++i;
+        }
+        // 检查符号
+        if(str[i] == '-')
+            flag = false;
+        if(str[i] == '+' || false == flag)
+            ++i;
+        // 数字检查
+        for(; i < str.size(); ++i)
+        {
+            int r = str[i] - '0';
+            // 此处可用 isdigit(str[i]);
+            if(str[i] >= '0' && str[i] <= '9')
+            {
+                //INT_MAX=2147483647
+                if(res >  INT_MAX/10 || (INT_MAX/10 == res && r > 7))
+                    return flag ? INT_MAX : INT_MIN;
+                res = 10 * res +r;
+            }else 
+                break;
+        }
+        return flag ? res : -res;
     }
 };
 // @lc code=end
