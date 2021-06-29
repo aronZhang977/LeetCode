443d53d2394987a9f162cdbd820d201f58243911
aronZhang977 <aron.zhang.coding@gmail.com>
2021-06-29 14:27:48 +0800

    0629

--- 168.excel表列名称.cpp
+++ 168.excel表列名称.cpp
@@ -8,7 +8,22 @@
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-
+        string ans;
+        while (columnNumber > 0)
+        {
+            int a0 = (columnNumber - 1) % 26 + 1;
+            ans += a0 - 1 + 'A';
+            columnNumber = (columnNumber - a0) /26;
+        }
+#if 0
+         while (columnNumber > 0) {
+            --columnNumber;
+            ans += columnNumber % 26 + 'A';
+            columnNumber /= 26;
+        }
+#endif
+        reverse(ans.begin(), ans.end());
+        return ans;        
     }
 };
 // @lc code=end
--- 171.excel表列序号.cpp
+++ 171.excel表列序号.cpp
@@ -1,4 +1,4 @@
-/*
+ /*
  * @lc app=leetcode.cn id=171 lang=cpp
  *
  * [171] Excel表列序号
@@ -8,7 +8,15 @@
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-
+        int number = 0;
+        long multiple = 1;
+        for(int  i = columnTitle.size() - 1; i >= 0; --i)
+        {
+            int k = columnTitle[i] - 'A' + 1;
+            number += k * multiple;
+            multiple *= 26;
+        }
+        return number;
     }
 };
 // @lc code=end
